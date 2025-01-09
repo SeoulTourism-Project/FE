e801d6ee2e9ce379b6923a9499ed86f8e72374f5
leeselra <tpffk2555@naver.com>
2025-01-09 15:16:00 +0900

    미완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Route, Routes } from 'react-router';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Cart from "./pages/Cart";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/signup' element={<Signup />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/cart" element={<Cart />} />
     </Routes>
   );
 };
