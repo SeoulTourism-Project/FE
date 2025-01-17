8454327e47d6261b0f777acb53624c0fade0ec80
Minjoo Huh <huhminjoo@naver.com>
2025-01-17 14:38:55 +0900

    feat: 캘린더 날짜와 마크 위치 조정
    - 날짜 타일의 상단에 날짜(일)를 두고, 마크를 그 밑으로 둔다.
    - display: flex, flex-direction: column을 줌으로써 날짜 폰트 크기가 변경되어도 마크의 위치는 날짜의 아래에 두게 된다.
    - 마크 크기 변경. 시스템 사이즈의 1.5배로 설정.

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -7,9 +7,7 @@ const Calendar = ({ selectedDate, onDateChange, schedules = [] }) => {
   const tileContent = ({ date }) => {
     const hasSchedule = schedules.includes(date.toISOString().split("T")[0]); // "YYYY-MM-DD" -> "YYYY-MM-DDTHH:mm:ss.sssZ"
 
-    return hasSchedule ? (
-      <div style={{ color: "yellow", fontWeight: "bold" }}>★</div>
-    ) : null;
+    return hasSchedule ? <div>⭐</div> : <div></div>;
   };
 
   const handleDateChange = (date) => {
@@ -35,6 +33,21 @@ const CalendarContainer = styled.div`
 
   .react-calendar__tile {
     height: 80px !important;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    position: relative;
+  }
+
+  .react-calendar__tile div {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    font-size: 1.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
