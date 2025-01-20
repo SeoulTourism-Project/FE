762ee6d45c000d4ecb778160fc0335eb47dfd054
Minjoo Huh <huhminjoo@naver.com>
2025-01-20 23:51:28 +0900

    refactor: ScheduleCard 컴포넌트를 분리하여 코드 구조 개선
    - Timetable에서 ScheduleCard 관련 내용 별도 파일로 분리.
    - 코드 가독성과 유지보수성을 높이기 위해서 구조 개선.

---
+++ src/components/ScheduleCard.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import styled from "styled-components";
+import { formatTime24 } from "../utils/koreaDateUtils"; // 필요한 유틸 함수 import
+
+const ScheduleCard = ({ schedule }) => {
+  return (
+    <CardContainer>
+      <ScheduleTime>
+        {formatTime24(schedule.scheduleDate)} -{" "}
+        {formatTime24(schedule.scheduleEndDate)}
+      </ScheduleTime>
+      <ScheduleItem>
+        <ScheduleImage src={schedule.image} alt={schedule.title} />
+        <ScheduleDetails>
+          <ScheduleName>{schedule.title}</ScheduleName>
+          <ScheduleAddress>{schedule.address}</ScheduleAddress>
+          <ScheduleMemo>
+            <p>메모</p>
+            {schedule.memo}
+          </ScheduleMemo>
+        </ScheduleDetails>
+      </ScheduleItem>
+    </CardContainer>
+  );
+};
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 190px;
+  margin-top: 2px;
+`;
+
+const ScheduleTime = styled.div`
+  flex: 0 0 auto;
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+const ScheduleItem = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1 1 auto;
+  width: 100%;
+  margin-left: 8px;
+
+  overflow: hidden;
+  border-radius: 5px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
+`;
+
+const ScheduleImage = styled.img`
+  aspect-ratio: 1 / 1;
+  height: 100%;
+  margin-right: 18px;
+  object-fit: cover;
+`;
+
+const ScheduleDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+
+  padding-top: 20px;
+  margin-right: 20px;
+`;
+
+const ScheduleName = styled.p`
+  font-size: 1.3rem;
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+
+const ScheduleAddress = styled.p`
+  color: gray;
+  margin-bottom: 20px;
+`;
+
+const ScheduleMemo = styled.div`
+  p {
+    font-weight: bold;
+    margin-bottom: 3px;
+  }
+`;
+
+export default ScheduleCard;
--- src/components/Timetable.jsx
+++ src/components/Timetable.jsx
@@ -5,6 +5,7 @@ import {
   formatTime24,
   convertToKoreaDate,
 } from "../utils/koreaDateUtils"; // 한국 시간대로 변경
+import ScheduleCard from "./ScheduleCard";
 
 const Timetable = ({ date, schedules }) => {
   // 선택된 날짜와 일치하는 일정 필터링
@@ -14,8 +15,7 @@ const Timetable = ({ date, schedules }) => {
 
   return (
     <TimeTableContainer>
-      <h2>{convertToKoreaDate(date).toDateString()}</h2>{" "}
-      {/* YYYY-MM-DD 형식으로 날짜 표시 */}
+      <h2>{convertToKoreaDate(date).toDateString()}</h2>
       {filteredSchedules.length > 0
         ? filteredSchedules.map((schedule, index) => (
             <ScheduleContainer key={index}>
@@ -23,24 +23,7 @@ const Timetable = ({ date, schedules }) => {
                 <Circle value={"black"} />
                 <Line />
               </Figure>
-              <ScheduleCard>
-                <ScheduleTime>
-                  {formatTime24(schedule.scheduleDate)} -{" "}
-                  {formatTime24(schedule.scheduleEndDate)}
-                </ScheduleTime>
-                <ScheduleItem>
-                  <ScheduleImage src={schedule.image} alt={schedule.title} />
-                  <ScheduleDetails>
-                    <ScheduleName>{schedule.title}</ScheduleName>
-                    <ScheduleAddress>{schedule.address}</ScheduleAddress>
-                    <ScheduleMemo>
-                      <p>메모</p>
-                      {schedule.memo}{" "}
-                      {/* 메모 40자 이내로 작성 (띄어쓰기 포함함) */}
-                    </ScheduleMemo>
-                  </ScheduleDetails>
-                </ScheduleItem>
-              </ScheduleCard>
+              <ScheduleCard schedule={schedule} /> {/* ScheduleCard 사용 */}
             </ScheduleContainer>
           ))
         : null}
@@ -97,82 +80,6 @@ const Line = styled.div`
 
   border-left: 1px solid black;
   height: 210px;
-`;
-
-const ScheduleCard = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  width: 100%;
-  height: 190px;
-  margin-top: 2px;
-`;
-
-const ScheduleTime = styled.div`
-  flex: 0 0 auto;
-  font-size: 1.2rem;
-  font-weight: bold;
-  margin-bottom: 20px;
-`;
-
-const ScheduleItem = styled.div`
-  display: flex;
-  align-items: center;
-  flex: 1 1 auto; // 남은 크기
-  width: 100%;
-  margin-left: 8px;
-
-  overflow: hidden;
-  border-radius: 5px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
-`;
-
-const ScheduleImage = styled.img`
-  aspect-ratio: 1 / 1;
-  height: 100%;
-  margin-right: 18px;
-  //   border-radius: 5px;
-  object-fit: cover; // conver: 자르기 / contain: 전체 보여주기
-`;
-
-const ScheduleDetails = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-
-  padding-top: 20px;
-  margin-right: 20px;
-`;
-
-const ScheduleName = styled.p`
-  font-size: 1.3rem;
-  font-weight: bold;
-
-  margin-bottom: 5px;
-`;
-
-const ScheduleAddress = styled.p`
-  color: gray;
-
-  margin-bottom: 20px;
-`;
-
-const ScheduleMemo = styled.div`
-  p {
-    font-weight: bold;
-    margin-bottom: 3px;
-  }
-`;
-
-const EmptySpace = styled.div`
-  margin: 0 0 30px 10px;
-
-  display: flex;
-  align-items: center;
-  height: 40px;
-
-  background: pink;
 `;
 
 const ScheduleAddContainer = styled.div`
