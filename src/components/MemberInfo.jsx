6e1fb9eb431219773782180570f1f38191bb49d5
Minjoo Huh <huhminjoo@naver.com>
2025-01-30 14:45:16 +0900

    feat: 회원 정보, 결제 내역 컴포넌트 생성 및 탭으로 연동
    - 탭으로 회원 정보, 결제 내역 구분.
    - 회원 정보 버튼 클릭 시, MemberInfo 컴포넌트 불러오기..
    - 결제 내역 버튼 클릭 시, PaymentHistory 컴포넌트 불러오기.

---
+++ src/components/MemberInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MemberInfo = () => {
+  return <div>회원 정보</div>;
+};
+
+export default MemberInfo;
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import MemberInfo from "../components/MemberInfo";
+import PaymentHistory from "../components/PaymentHistory";
+import styled from "styled-components";
 
 const MyPage = () => {
-  return <div>마이페이지</div>;
+  const [activeTab, setActiveTab] = useState("memberInfo");
+
+  return (
+    <Container>
+      <Title>개인 정보</Title>
+      <Tabs>
+        <TabButton
+          active={activeTab === "memberInfo"}
+          onClick={() => setActiveTab("memberInfo")}
+        >
+          회원 정보
+        </TabButton>
+        <p>|</p>
+        <TabButton
+          active={activeTab === "payHistory"}
+          onClick={() => setActiveTab("payHistory")}
+        >
+          결제 내역
+        </TabButton>
+      </Tabs>
+      <div>
+        {activeTab === "memberInfo" && <MemberInfo />}
+        {activeTab === "payHistory" && <PaymentHistory />}
+      </div>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 1280px;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  text-align: center;
+  margin-bottom: 40px;
+`;
+
+const Tabs = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+
+  p {
+    font-size: 2rem;
+    margin: 0 40px;
+  }
+`;
+
+const TabButton = styled.button`
+  background: ${(props) => (props.active ? "gray" : "#fff")};
+  color: ${(props) => (props.active ? "#fff" : "#000")};
+  font-size: 1rem;
+  border: none;
+  padding: 10px 20px;
+  margin: 0 5px;
+  cursor: pointer;
+  border-radius: 5px;
+  &:hover {
+    background: ${(props) => (props.active ? "#555" : "#ddd")};
+  }
+`;
 
 export default MyPage;
