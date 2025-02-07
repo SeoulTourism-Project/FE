8a6eb6272128340f27fed2f2a1b06497c9feb5ac
Minjoo Huh <huhminjoo@naver.com>
2025-02-07 20:16:41 +0900

    refactor: 로그인 styled-components 위치 이동

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
-import { Link } from 'react-router';
+import React from "react";
+import styled from "styled-components";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { Link } from "react-router";
+
+const Login = () => {
+  const clientId = "YOUR_GOOGLE_CLIENT_ID"; // Google Cloud에서 발급받은 Client ID
+
+  const handleGoogleLoginSuccess = (response) => {
+    console.log("Google Login Success", response);
+    // 원하는 로직 추가: 예) 백엔드로 토큰 전송
+  };
+
+  const handleGoogleLoginFailure = (response) => {
+    console.error("Google Login Failure", response);
+  };
+
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <LoginContainer>
+        <Title>로그인</Title>
+        <Form>
+          <Label htmlFor="email">이메일</Label>
+          <Input type="email" id="email" placeholder="이메일" />
+
+          <Label htmlFor="password">비밀번호</Label>
+          <Input type="password" id="password" placeholder="비밀번호" />
+
+          <Button bgColor="#7a7a7a">로그인</Button>
+          <GoogleLogin
+            onSuccess={handleGoogleLoginSuccess}
+            onError={handleGoogleLoginFailure}
+            render={(renderProps) => (
+              <Button
+                bgColor="#fff"
+                style={{ color: "#7a7a7a", border: "1px solid #ccc" }}
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+              >
+                Google 간편로그인
+              </Button>
+            )}
+          />
+          <LinkButton to="/signup">회원가입</LinkButton>
+        </Form>
+      </LoginContainer>
+    </GoogleOAuthProvider>
+  );
+};
 
 const LoginContainer = styled.div`
   width: 300px;
@@ -43,7 +88,7 @@ const Button = styled.button`
   border-radius: 4px;
   cursor: pointer;
   color: #fff;
-  background-color: ${(props) => props.bgColor || '#7a7a7a'};
+  background-color: ${(props) => props.bgColor || "#7a7a7a"};
   &:hover {
     opacity: 0.9;
   }
@@ -64,50 +109,5 @@ const LinkButton = styled(Link)`
     opacity: 0.9;
   }
 `;
-
-const Login = () => {
-  const clientId = "YOUR_GOOGLE_CLIENT_ID"; // Google Cloud에서 발급받은 Client ID
-
-  const handleGoogleLoginSuccess = (response) => {
-    console.log("Google Login Success", response);
-    // 원하는 로직 추가: 예) 백엔드로 토큰 전송
-  };
-
-  const handleGoogleLoginFailure = (response) => {
-    console.error("Google Login Failure", response);
-  };
-
-  return (
-    <GoogleOAuthProvider clientId={clientId}>
-      <LoginContainer>
-        <Title>로그인</Title>
-        <Form>
-          <Label htmlFor="email">이메일</Label>
-          <Input type="email" id="email" placeholder="이메일" />
-          
-          <Label htmlFor="password">비밀번호</Label>
-          <Input type="password" id="password" placeholder="비밀번호" />
-
-          <Button bgColor="#7a7a7a">로그인</Button>
-          <GoogleLogin
-            onSuccess={handleGoogleLoginSuccess}
-            onError={handleGoogleLoginFailure}
-            render={(renderProps) => (
-              <Button
-                bgColor="#fff"
-                style={{ color: '#7a7a7a', border: '1px solid #ccc' }}
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
-              >
-                Google 간편로그인
-              </Button>
-            )}
-          />
-          <LinkButton to="/signup">회원가입</LinkButton>
-        </Form>
-      </LoginContainer>
-    </GoogleOAuthProvider>
-  );
-};
 
 export default Login;
