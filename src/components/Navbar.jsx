71f38c148d0bcafcde443fa7504a5d068b5de7d4
Minjoo Huh <huhminjoo@naver.com>
2025-02-09 09:47:29 +0900

    feat: 로그인 시, header Navbar 변경 및 사용자 이름 추가
    - session storage를 확인하여 access token 여부 확인.
    - 있으면, 로그인이 되어 있는 상태이므로 `환영합니다 {유저이름}`을 출력
    - 없으면, 로그인, 회원가입 버튼을 출력.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,58 @@
-import { Link } from 'react-router';
-import styled from 'styled-components';
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { getUserNameFromToken } from "../utils/decodeToken";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("accessToken");
+    const storedUsername = getUserNameFromToken();
+    console.log(storedUsername);
+    if (token) {
+      setIsLoggedIn(true);
+      setUsername(storedUsername || "사용자");
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
   return (
-    <nav>
-      <StyledLink to={'/login'}>로그인</StyledLink>
-      <StyledLink to={'/signup'}>회원가입</StyledLink>
-    </nav>
+    <NavContainer>
+      {!isLoggedIn ? (
+        // 로그인하지 않은 경우 (로그인 & 회원가입 버튼)
+        <>
+          <StyledLink to="/login">로그인</StyledLink>
+          <StyledLink to="/signup">회원가입</StyledLink>
+        </>
+      ) : (
+        // 로그인한 경우 (마이페이지, 찜한 여행지, 캘린더, 로그아웃 등)
+        <WelcomeText>환영합니다 {username}님</WelcomeText>
+      )}
+    </NavContainer>
   );
 };
 
 export default Navbar;
+
+const NavContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
 
 const StyledLink = styled(Link)`
   font-size: 1.2rem;
   font-weight: bold;
   color: black;
   text-decoration: none;
+`;
 
-  &:last-child {
-    margin-left: 20px;
-  }
+const WelcomeText = styled.div`
+  font-size: 1.2rem;
+  font-weight: bold;
+  cursor: pointer;
 `;
