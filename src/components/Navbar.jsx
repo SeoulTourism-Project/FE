2445b455975bea8f30481c5db3d18228ec160f4e
Minjoo Huh <huhminjoo@naver.com>
2025-02-10 19:55:09 +0900

    feat: react-router-dom을 react-router로 변경

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
